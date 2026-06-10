2fd3499172e50b3758ebbbdcdf30880d8e850597
Karl Hallsby <karl@hallsby.com>
2026-06-10 17:37:56 -0500

    Add utval CSR to hold onto vaddr of load/store page faults

--- src/main/scala/rocket/CSR.scala
+++ src/main/scala/rocket/CSR.scala
@@ -608,6 +608,7 @@ class CSRFile(
   val reg_starget = RegInit(0.U(vaddrBitsExtended.W))
   val reg_uepc = Reg(UInt(vaddrBitsExtended.W))
   val reg_ucause = Reg(Bits(xLen.W))
+  val reg_utval = Reg(UInt(vaddrBitsExtended.W))
   val reg_uscratch = Reg(Bits(xLen.W))
   val reg_salready_handling = RegInit(false.B)
   /* End of Pipelined Interrupts/Exceptions. */
@@ -843,6 +844,7 @@ class CSRFile(
       read_mapping += CSRs.uscratch -> reg_uscratch
       read_mapping += CSRs.uepc -> readEPC(reg_uepc).sextTo(xLen)
       read_mapping += CSRs.ucause -> reg_ucause
+      read_mapping += CSRs.utval -> reg_utval.sextTo(xLen)
       read_mapping += CSRs.salready_handling -> reg_salready_handling
       read_mapping += CSRs.ualready_handling -> reg_salready_handling
       read_mapping += CSRs.fflags_care -> reg_fflags_care
@@ -1171,6 +1173,7 @@ class CSRFile(
         new_prv := PRV.U.U
         reg_ucause := cause
         reg_uepc := epc
+        reg_utval := tval
         reg_salready_handling := true.B
       }.otherwise {
         new_prv := PRV.S.U
@@ -1499,6 +1502,7 @@ class CSRFile(
         when (decoded_addr(CSRs.starget))  { reg_starget := wdata }
         when (decoded_addr(CSRs.uscratch))  { reg_uscratch := wdata }
         when (decoded_addr(CSRs.uepc))      { reg_uepc := formEPC(wdata) }
+        when (decoded_addr(CSRs.utval))    { reg_utval := wdata }
         when (decoded_addr(CSRs.fflags_care))  { reg_fflags_care := wdata }
         when (decoded_addr(CSRs.salready_handling))  {
           when (reg_mstatus.prv >= PRV.S.U) {
